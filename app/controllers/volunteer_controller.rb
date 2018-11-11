20edf9d89541b26653f1c0efad99313e808329a6
SMartinSci <sm674@cornell.edu>
2018-11-11 13:13:47 -0800

    Render a volunteer log form, post forms and show route

---
+++ app/controllers/volunteer_controller.rb
@@ -0,0 +1,22 @@
+class VolunteerController < ApplicationController
+    
+    get '/volunteer_logs/new' do
+        erb :'/volunteer_logs/new'
+    end
+    
+    post '/volunteer_logs' do
+        if logged_in?
+            @user = current_user
+            session[:user_id] = @user.id
+            @volunteers = Volunteer.all
+        erb :'/volunteers/index'
+
+        else
+            redirect to '/login'
+    end
+
+    get '/volunteer_logs/:id' do
+        @volunteer_log = VolunteerLog.find(params[:id])
+        erb :'/volunteer_logs/show'
+    end
+  end   
