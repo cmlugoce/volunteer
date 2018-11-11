1c35f612bdd8bb52dbf2e16b015bca92328d473f
SMartinSci <sm674@cornell.edu>
2018-11-11 12:55:55 -0800

    Fix bad repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "my_application_volunteer_journal_app"
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      user = User.find_by(id: session[:user_id])
+      return user if user
+    end
+
+  end
 end
