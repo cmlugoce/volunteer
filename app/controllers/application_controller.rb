90e8a5a7b7ec604f2065527fcf99a1fa7b77ed70
Sherry M <40738084+SMartinSci@users.noreply.github.com>
2018-11-15 10:52:47 -0500

    Update current user method with session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,9 +22,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      user = User.find_by(id: session[:user_id])
-      return user if user
-    end
-
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end 
   end
 end
