8308822f4b25d626b3d739c21af3048f0b9cdc09
SMartinSci <sm674@cornell.edu>
2018-11-15 12:44:00 -0800

    Add signup route to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,30 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      flash[:signup_message] = "Please fill out all required fields (username, email, and password)"
+      redirect '/signup'
+    end
+
+    if User.find_by(username: params[:username])
+      flash[:failure_message] = "Username is Taken"
+      redirect '/signup'
+    elsif 
+      params[:email] != params[:confirm_email]
+      flash[:message] = "This email already exists. Please enter a new email or log in to continue."
+      redirect to '/signup'
+    elsif 
+      params[:password] != params[:confirm_password]
+      flash[:failure] = "Passwords do not match"
+      redirect '/signup'
+    else 
+      @user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/entries'
+  end
+end
+
   get '/login' do
     erb :login
   end
@@ -22,16 +46,10 @@ class UsersController < ApplicationController
     end
   end
 
-  #get signup
-
-  #post signup
-
   get '/users/:id' do
     @user = User.find_by(id: params[:id])
     erb :'/users/show'
   end
-
-  
 
   get '/logout' do
     session.clear
