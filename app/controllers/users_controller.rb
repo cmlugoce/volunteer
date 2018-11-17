b84087464b31ca040941f4225fbcc88e4548f820
SMartinSci <sm674@cornell.edu>
2018-11-17 14:28:49 -0800

    Rework controller routes in entrys controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,41 +2,42 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if logged_in?
-       redirect to '/entrys' 
+      redirect to '/entrys' 
     else
       erb :'users/signup'
     end
   end
 
   post '/signup' do
-    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-      redirect '/entrys'
-    if 
-       flash[:signup_message] = "Please fill out all required fields (username, email, and password)"
-      redirect '/signup'
-    elsif
-      !User.new(:username => params[:username], :password => params[:password]).valid?
-      flash[:failure_message] = "Username is taken, please choose another username."
-      redirect '/signup'
-    elsif 
-      params[:email] != params[:email]
-      flash[:message] = "This email already exists. Please enter a new email or log in to continue."
-      redirect to '/signup'
-    elsif 
-      params[:password] != params[:password]
-      flash[:failure] = "Passwords do not match"
-      redirect '/signup'  
-    elsif params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect '/signup'  
-  end
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+    session[:user_id] = @user.id
+      redirect to '/entrys'
+    # if 
+    #   flash[:signup_message] = "Please fill out all required fields (username, email, and password)"
+    #   redirect '/signup'
+    # elsif
+    #   !User.new(:username => params[:username], :password => params[:password]).valid?
+    #   flash[:failure_message] = "Username is taken, please choose another username."
+    #   redirect '/signup'
+    # elsif 
+    #   params[:email] != params[:email]
+    #   flash[:message] = "This email already exists. Please enter a new email or log in to continue."
+    #   redirect to '/signup'
+    # elsif 
+    #   params[:password] != params[:password]
+    #   flash[:failure] = "Passwords do not match"
+    #   redirect '/signup'  
+    # elsif params[:username] == "" || params[:email] == "" || params[:password] == ""
+    #   redirect '/signup'  
+  # end
 end
 
   get '/login' do
     if !logged_in?
       erb :'/users/login'
     else
-      redirect to 'entrys'
+      redirect to '/entrys'
     end
   end
 
