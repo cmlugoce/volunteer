52e637d7ed26413eb45603d03586f58cf6410cb1
SMartinSci <sm674@cornell.edu>
2018-11-17 13:19:32 -0800

    modify user in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many :volunteer_logs
+  has_many :logs
     validates_uniqueness_of :username, :email
     validates_presence_of :username, :email, :password
   
