22f9976976b79722a04aa387597fa0e17203a7f6
SMartinSci <sm674@cornell.edu>
2018-11-17 16:20:38 -0800

    Add relationships to models

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,6 @@
 class Log < ActiveRecord::Base
 
   belongs_to :user
+  has_many :entries
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+ 
   has_secure_password
-
+  
+  has_many :entries, through: :logs
   has_many :logs
     validates_uniqueness_of :username, :email
     validates_presence_of :username, :email, :password
