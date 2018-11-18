7df2d23eeb504f0b0347f3ddfcc687a5393d9cfa
cmlugoce <cmlugoce@syr.edu>
2018-11-17 20:22:44 -0800

    remove slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,9 @@ class User < ActiveRecord::Base
   has_many :entries, through: :logs
   has_many :logs
     validates_uniqueness_of :username, :email
-    validates_presence_of :username, :email, :password
+    validates_presence_of :username, :email
   
 
-  def slug
-    self.username.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
-  end
+  
   
 end
